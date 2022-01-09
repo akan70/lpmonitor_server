2a967a94e5c9b8cfd61b08d317dd4ba195bf787d
Han Chung Hyok <PerfectOnly86702@gmail.com>
2022-01-09 14:28:48 +0900

    add ping api

--- routes/record.js
+++ routes/record.js
@@ -12,6 +12,9 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
+recordRoutes.route("/ping").get(function (req, res) {
+  res.json({success:"successfully connect!"});
+});
 
 // This section will help you get a list of all the records.
 recordRoutes.route("/getCoinList").get(function (req, res) {
